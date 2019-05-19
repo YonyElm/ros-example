eaa123c5ed37b46e277954885a35019df574ac7e
TechyE <yony.elm@gmail.com>
2019-05-18 23:22:58 -0700

    first commit cycle

---
+++ host_machine/requirements.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Gstreamer
+apt-get install -y libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
+
+# V4l2loopback
+sudo apt install -y v4l2loopback-dkms
+
+
